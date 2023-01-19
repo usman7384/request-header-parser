4cbe6a57a54c5b1be163a7576137293f9afc1ee5
usman7384 <mu3931368@gmail.com>
2023-01-19 16:19:06 +0500

    header parser

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
+const whoamiRouter = require("./whoamiRouter");
 var app = express();
+app.use('/', whoamiRouter);
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -25,6 +18,6 @@ app.get('/api/hello', function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, '0.0.0.0',function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
