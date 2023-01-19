7df0b9578c83866c6bfb8e0c4f0926a5f41ec3ae
usman7384 <mu3931368@gmail.com>
2023-01-19 17:18:45 +0500

    headerparser

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 require('dotenv').config();
+
 var express = require('express');
-const whoamiRouter = require("./whoamiRouter");
 var app = express();
-app.use('/', whoamiRouter);
+
+app.get("/api/whoami", function (req, res) {
+  const ip = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.send({ip: ip, language: language, software: software});
+})
 
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
-app.get('/', function (req, res) {
+
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, '0.0.0.0',function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
